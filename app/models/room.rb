6ebf5070f4f760825733118849c3a14326633e48
TH <tharada0854@gmail.com>
2018-10-24 13:13:00 +0000

    バリデーション、アソシエーション設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,10 @@
 class Room < ApplicationRecord
+  has_many :station
+
+  validates :room_name, presence: true, length: { in: 1..50 }, uniqueness: true
+  validates :price, presence: true, length: { in: 1..4 }, numericality: true
+  validates :adress, presence: true, length: { in: 1..100 }
+  validates :built_years, presence: true, numericality: true
+
+  # mount_uploader :image, ImageUploader
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,8 @@
 class Station < ApplicationRecord
+  belongs_to :room
+
+  validates :station_name, presence: true, length: { in: 1..20 }
+  validates :line_to_station, presence: true, length: { in: 1..20 }
+  validates :minutes_on_foot, presence: true, length: { in: 1..2 }, numericality: true
+
 end
